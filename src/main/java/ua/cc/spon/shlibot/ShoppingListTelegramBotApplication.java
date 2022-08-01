36f3a9ef19253dc84b350df413817f81f86a55ad
Sergiy Ponomarenko <s.stefaniv@gmail.com>
2022-08-01 14:17:27 +0300

    SHLIBOT-0: added SpringBoot skeleton to the project

---
+++ src/main/java/ua/cc/spon/shlibot/ShoppingListTelegramBotApplication.java
@@ -0,0 +1,13 @@
+package ua.cc.spon.shlibot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppingListTelegramBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoppingListTelegramBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ua/cc/spon/shlibot/ShoppingListTelegramBotApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.cc.spon.shlibot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingListTelegramBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
